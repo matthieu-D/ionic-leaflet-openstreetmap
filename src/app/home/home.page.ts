64f5581fc906d779d86f9e7bb81c5ce020b5792f
Matthieu Drula <matthieu.drula.hs@gmail.com>
2019-05-01 15:38:46 +0200

    all working

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,9 @@
 import { Component } from '@angular/core';
+import { Platform } from '@ionic/angular';
+import { Router } from '@angular/router';
+import { Map, tileLayer, marker, icon } from 'leaflet';
+import { Http } from '@angular/http';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-home',
@@ -6,5 +11,36 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  constructor(public http: Http,
+              public plt: Platform,
+              public router: Router) {}
 
+  ngAfterViewInit() {
+    this.plt.ready().then(() => {
+      this.http.get('https://oghuxxw1e6.execute-api.us-east-1.amazonaws.com/dev')
+      .pipe(map(res => res.json()))
+      .subscribe(restaurants => this.initMap(restaurants));
+    });
+  }
+
+  initMap(restaurants) {
+    const map = new Map('map').setView([33.6396965, -84.4304574], 23);
+
+    tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    }).addTo(map);
+    
+    const customMarkerIcon = icon({
+      iconUrl: 'assets/images/custom-marker.png',
+      iconSize: [64, 64], // size of the icon
+      popupAnchor: [0, -20]
+    });
+
+    restaurants.forEach((restaurant) => {
+      marker([restaurant.position.lat, restaurant.position.lgn], {icon: customMarkerIcon})
+      .bindPopup(`<b>${restaurant.title}</b>`, { autoClose: false })
+      .on('click', () => this.router.navigateByUrl('/restaurant'))
+      .addTo(map).openPopup();
+    });
+  }
 }
